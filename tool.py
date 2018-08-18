68206750c5292dc09ab2e1b64bcfe344562c7ea2
chenyongchao <chen_yongchao@126.com>
2018-08-18 15:15:13 +0800

    chen v1.2

--- tool.py
+++ tool.py
@@ -2,6 +2,10 @@ def add(*args):
     l = len(args)
     result = 0
     for i in range(l):
-        result += args[i]
+        if hasattr(args[i],'index'):
+            for j in range(len(args[i])):
+                result += args[i][j]
+        else:
+            result += args[i]
     return result
-print(add(1,2))
+print(add(1,2,[3,4]))
